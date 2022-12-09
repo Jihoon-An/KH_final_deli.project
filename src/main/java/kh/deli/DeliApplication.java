fc5cd77bf27bbc19d285f710e3514b7a725970bb
DESKTOP-50SGODH\user1 <nhd5607@gmail.com>
2022-12-09 17:37:18 +0900

    first commit

---
+++ src/main/java/kh/deli/DeliApplication.java
@@ -0,0 +1,13 @@
+package kh.deli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kh/deli/DeliApplicationTests.java
@@ -0,0 +1,13 @@
+package kh.deli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
