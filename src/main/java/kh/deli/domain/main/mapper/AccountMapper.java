6ee919e9852fbab5ef62544ee811459ec407ee52
MinS2op <minseop9908@naver.com>
2022-12-12 12:02:07 +0900

    feat: AccountMapper 인터페이스 + Account-mapper.xml 추가

---
+++ src/main/java/kh/deli/domain/main/mapper/AccountMapper.java
@@ -0,0 +1,16 @@
+package kh.deli.domain.main.mapper;
+
+import kh.deli.global.entity.OwnerDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Service;
+
+@Service
+@Mapper
+public interface AccountMapper {
+
+    public void insert(@Param("account") OwnerDTO owner);
+
+
+
+}
