7a7976ce9435947ae6e7b8999c899931db6fc070
hye <hezuc12@gmail.com>
2022-12-13 11:23:20 +0900

    save point

--- src/main/java/kh/deli/domain/main/mapper/AccountMapper.java
+++ src/main/java/kh/deli/domain/main/mapper/AccountMapper.java
@@ -12,16 +12,20 @@ import java.util.Map;
 @Mapper
 public interface AccountMapper {
 
-    public void insert(@Param("account") AccountDTO accountDTO);
+    void insert(@Param("account") AccountDTO accountDTO);
 
-    public String findByEmail(@Param("email") String email);
+    String findByEmail(@Param("email") String email);
 
-    public void insertOwner(@Param("acc") AccountDTO acc);
+    void insertOwner(@Param("acc") AccountDTO acc);
 
     @Select("SELECT ACC_SEQ.NEXTVAL FROM DUAL")
-    public int getAccSeq();
+    int getAccSeq();
 
-    public int login(Map<String, String> param);
+    int login(Map<String, String> param);
 
     void memberSignUp(AccountDTO accountDTO);
+
+    void kakaoSignUp(AccountDTO accountDTO);
+
+    String findByAccToken(String acc_token);
 }
