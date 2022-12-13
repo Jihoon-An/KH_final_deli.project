7661e211427dc3447e66f9a449e4493a8b2e6f10
dOcOb <abg1418@gmail.com>
2022-12-13 14:09:31 +0900

    feat: 사업자 회원가입 비밀번호 암호화

--- src/main/java/kh/deli/domain/main/service/OwnerSignUpService.java
+++ src/main/java/kh/deli/domain/main/service/OwnerSignUpService.java
@@ -4,6 +4,7 @@ import kh.deli.domain.main.mapper.AccountMapper;
 import kh.deli.domain.main.mapper.OwnerSignUpMapper;
 import kh.deli.global.entity.AccountDTO;
 import kh.deli.global.entity.OwnerDTO;
+import kh.deli.global.util.Encryptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,8 +22,12 @@ public class OwnerSignUpService {
         int accSeq = accountMapper.getAccSeq();
         owner.setAcc_seq(accSeq);
         owner.setOwner_card_img("testSet");
-        account.setAcc_seq(accSeq);
+
         ownerSignUpMapper.insert(owner);
+        
+        account.setAcc_seq(accSeq);
+        account.setAcc_pw(Encryptor.getSHA512(account.getAcc_pw()));
+
         accountMapper.insertOwner(account);
     }
 }
