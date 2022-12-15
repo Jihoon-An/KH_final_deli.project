24e97a8ab4692959470fffe157deb548e870cdcf
hye <hezuc12@gmail.com>
2022-12-15 20:16:02 +0900

    fixed: 우편번호 오류 수정

--- src/main/java/kh/deli/domain/member/store/service/StoreStoreService.java
+++ src/main/java/kh/deli/domain/member/store/service/StoreStoreService.java
@@ -1,9 +1,12 @@
 package kh.deli.domain.member.store.service;
 
 import kh.deli.domain.member.store.mapper.StoreStoreMapper;
+import kh.deli.global.entity.AddressDTO;
 import kh.deli.global.entity.StoreDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
@@ -13,5 +16,8 @@ public class StoreStoreService {
 
     public void  insertStore(StoreDTO dto) {
          storeMapper.insertStore(dto);
+    }
+
+    public List<AddressDTO> selectAll(int acc_seq) {
     }
 }
--- src/main/webapp/WEB-INF/views/member/store/storeAdd.jsp
+++ src/main/webapp/WEB-INF/views/member/store/storeAdd.jsp
@@ -347,6 +347,7 @@
 
                 // 우편번호와 주소 정보를 해당 필드에 넣는다.
                 document.getElementById('postcode').value = data.zonecode;
+                document.getElementById("add1").value = data.roadAddress;
                 document.getElementById("add1").value = data.jibunAddress;
                 // 커서를 상세주소 필드로 이동한다.
                 document.getElementById("add2").focus();
