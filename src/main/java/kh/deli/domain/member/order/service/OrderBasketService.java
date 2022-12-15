cb0fd998a0e0f6a9369e6b9df7b00bc0e4a2e348
MinS2op <114593306+MinS2op@users.noreply.github.com>
2022-12-15 12:37:40 +0900

    feat: 메인페이지 임시 리모트 기능 / 장바구니 Controller, Mapper 생성

---
+++ src/main/java/kh/deli/domain/member/order/service/OrderBasketService.java
@@ -0,0 +1,12 @@
+package kh.deli.domain.member.order.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class OrderBasketService {
+
+
+
+}
