e4790bbdd118ffeb8035fad8cfd306098b50149d
hye <hezuc12@gmail.com>
2022-12-18 06:40:03 +0900

    refactor: 식당검색구현(100%)/배달기사전용페이지구현(90%)

--- src/main/java/kh/deli/domain/member/order/controller/OrderDeliveryDtlController.java
+++ src/main/java/kh/deli/domain/member/order/controller/OrderDeliveryDtlController.java
@@ -1,4 +1,30 @@
 package kh.deli.domain.member.order.controller;
 
+import kh.deli.domain.member.order.dto.OrdererInfoDTO;
+import kh.deli.domain.member.order.dto.PayInfoDTO;
+import kh.deli.domain.member.order.dto.StoreInfoDTO;
+import kh.deli.domain.member.order.service.OrderOrdersService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@AllArgsConstructor
+@RequestMapping("deliveryDtl")
 public class OrderDeliveryDtlController {
+    private final OrderOrdersService orderOrdersService;
+    @RequestMapping("")
+    public String toDeliveryDtl(Model model) throws Exception {
+
+        int order_seq = 9;
+        StoreInfoDTO storeInfoDTO = orderOrdersService.storeInfo(order_seq);
+        OrdererInfoDTO ordererInfoDTO =orderOrdersService.ordererInfo(order_seq);
+        PayInfoDTO payInfoDTO=orderOrdersService.payInfo(order_seq);
+        model.addAttribute("storeInfoDTO",storeInfoDTO);
+        model.addAttribute("ordererInfoDTO",ordererInfoDTO);
+        model.addAttribute("payInfoDTO",payInfoDTO);
+
+        return "member/order/deliveryDtl";
+    }
 }
--- src/main/webapp/WEB-INF/views/member/order/deliveryDtl.jsp
+++ src/main/webapp/WEB-INF/views/member/order/deliveryDtl.jsp
@@ -16,10 +16,35 @@
     <div class="container">
         <h1>고객배달영수증</h1>
         <h4>주문번호</h4>
-        <div>1233546456</div>
-        <h4>주문번호</h4>
-        <div>1233546456</div>
+        <div>${storeInfoDTO.order_seq}</div>
+        <h4>주문시간</h4>
+        <div>${storeInfoDTO.order_date}</div>
+        <h4>주문내역</h4>
+        <div>메뉴 출력</div>
+        <hr>
+        <h1>주문자정보</h1>
+        <h4>연락처</h4>
+        <div>${ordererInfoDTO.mem_phone}</div>
+        <h4>주소</h4>
+        <div>${ordererInfoDTO.address_add_detail1} ${ordererInfoDTO.orders_add_detail2}</div>
+        <h4>배송요청사항</h4>
+        <div>${ordererInfoDTO.order_rider_req}</div>
+        <hr>
+        <h1>식당정보</h1>
+        <h4>식당명</h4>
+        <div>${storeInfoDTO.store_name}</div>
+        <h4>연락처</h4>
+        <div>${storeInfoDTO.store_phone}</div>
+        <h4>주소</h4>
+        <div>${storeInfoDTO.store_add_detail1} ${storeInfoDTO.store_add_detail2}</div>
+        <hr>
+        <button>배달완료</button>
+        <hr>
+        누르면 확인 창 한번 더 뜨고 완료 누르면 controller에서 배달완료로 변경 됨
     </div>
+    배달 예상 시간 : ${storeInfoDTO.store_deli_time}분
+
+
 </main>
 </body>
 </html>
