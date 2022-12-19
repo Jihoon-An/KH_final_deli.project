1c9b86ab7a23885ef7375f7d820935cc9310e20a
nk <nhd5607@gmail.com>
2022-12-19 21:32:11 +0900

    fix: 식당상세메뉴수정,OwnerOwner mapper이름 수정

--- src/main/java/kh/deli/domain/member/myPage/service/MemberReviewService.java
+++ src/main/java/kh/deli/domain/member/myPage/service/MemberReviewService.java
@@ -54,8 +54,8 @@ public class MemberReviewService {
     }
 
     //리뷰출력
-    public List<StoreReviewDTO> getReviews() throws Exception{
-        return MyPageReviewMapper.getReviews();
+    public List<StoreReviewDTO> getReviews(int store_seq) throws Exception{
+        return MyPageReviewMapper.getReviews(store_seq);
     }
 
 }
