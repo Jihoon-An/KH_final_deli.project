768383f927833ccdb2715a26ee0e8fe356ad7965
HYB <dob.zivot20@gmail.com>
2022-12-20 21:57:19 +0900

    feat: 통계구현중

---
+++ src/main/java/kh/deli/domain/owner/batch/job/SimpleJobConfiguration.java
@@ -0,0 +1,11 @@
+package kh.deli.domain.owner.batch.job;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j // log 사용을 위한 lombok 어노테이션
+@RequiredArgsConstructor  // 생성자 DI를 위한 lombok 어노테이션
+@Configuration
+public class SimpleJobConfiguration {
+}
