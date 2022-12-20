55903c6e65062a8e4e3c01edd34b8a8f16bf83d8
dOcOb <abg1418@gmail.com>
2022-12-20 20:33:28 +0900

    feat: Member/menu/detail (메뉴상세, 장바구니 담기)

--- src/main/java/kh/deli/domain/member/store/mapper/StoreReviewMapper.java
+++ src/main/java/kh/deli/domain/member/store/mapper/StoreReviewMapper.java
@@ -1,10 +1,12 @@
 package kh.deli.domain.member.store.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Map;
 
+@Repository
 @Mapper
 public interface StoreReviewMapper {
 
