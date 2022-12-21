1e466e152f609d9e8c2c246c59b039c75f179fb4
MinS2op <114593306+MinS2op@users.noreply.github.com>
2022-12-21 16:58:48 +0900

    save 12.21 16:58

--- src/main/java/kh/deli/domain/member/order/controller/BasketController.java
+++ src/main/java/kh/deli/domain/member/order/controller/BasketController.java
@@ -1,5 +1,6 @@
 package kh.deli.domain.member.order.controller;
 
+import com.google.gson.Gson;
 import kh.deli.domain.member.order.dto.OrderBasketDTO;
 import kh.deli.domain.member.order.dto.OrderBasketMenuDTO;
 import kh.deli.domain.member.order.service.OrderBasketService;
@@ -7,8 +8,13 @@ import kh.deli.global.entity.StoreDTO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.List;
 
 @Controller
@@ -17,19 +23,28 @@ import java.util.List;
 public class BasketController {
 
     private final OrderBasketService basketService;
+    private final Gson gson;
 
     @RequestMapping("")
-    public String toBasket(Model model) throws Exception {
+    public String toBasket(@CookieValue(value = "basket", required = false) String basket, Model model) throws Exception {
+
+
+        OrderBasketDTO basketDTO = gson.fromJson(basket, OrderBasketDTO.class);
+
+//        System.out.println(basket);
+//        System.out.println(basketDTO);
+
+
 
         OrderBasketDTO sampleBasket = basketService.getSample();
 
-        StoreDTO storeDTO = sampleBasket.getStore();
-        List<OrderBasketMenuDTO> menuList = sampleBasket.getMenuList();
-        int totalPrice = sampleBasket.getTotalPrice();
-        int deliveryTip = sampleBasket.getStore().getStore_deli_tip();
-        int totalCount = basketService.getTotalCount(sampleBasket.getMenuList());
+        StoreDTO storeDTO = basketDTO.getStore();
+        List<OrderBasketMenuDTO> menuList = basketDTO.getMenuList();
+        int totalPrice = basketDTO.getTotalPrice();
+        int deliveryTip = basketDTO.getStore().getStore_deli_tip();
+        int totalCount = basketService.getTotalCount(basketDTO.getMenuList());
 
-        model.addAttribute("basket", sampleBasket);
+        model.addAttribute("basket", basketDTO);
         model.addAttribute("totalPrice", totalPrice);
         model.addAttribute("payAmount", (totalPrice + deliveryTip));
         model.addAttribute("totalCount", totalCount);
@@ -47,4 +62,15 @@ public class BasketController {
     }
 
 
+    private void setBasketCookie(String basket, HttpServletResponse response) throws UnsupportedEncodingException {
+        basket = URLEncoder.encode(basket, "utf-8");
+        Cookie cookie = new Cookie("basket", basket);
+
+        cookie.setMaxAge(60 * 60 * 24 * 14);//14일
+        cookie.setPath("/");
+
+        response.addCookie(cookie);
+    }
+
 }
+
