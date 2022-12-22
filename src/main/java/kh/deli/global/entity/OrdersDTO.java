9b3abb3699025d2825904c0b0c16a4b51fea1fc7
dOcOb <abg1418@gmail.com>
2022-12-22 14:34:49 +0900

    fix: int -> Integer

--- src/main/java/kh/deli/global/entity/OrdersDTO.java
+++ src/main/java/kh/deli/global/entity/OrdersDTO.java
@@ -10,22 +10,22 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrdersDTO {
-    private int order_seq;
-    private int acc_seq;
-    private int store_seq;
+    private Integer order_seq;
+    private Integer acc_seq;
+    private Integer store_seq;
     private String menu_list;
-    private int order_price;
+    private Integer order_price;
     Timestamp order_date;
-    private int add_seq;
+    private Integer add_seq;
     private String add_detail2;
     private String order_phone;
     private String order_status;
     private String order_disposable;
     private String order_rider_req;
     private String pay_method;
-    private int cp_seq;
-    private int order_point;
+    private Integer cp_seq;
+    private Integer order_point;
     private String order_store_req;
-    private int delivery_tip;
-    private int pay_price;
+    private Integer delivery_tip;
+    private Integer pay_price;
 }
