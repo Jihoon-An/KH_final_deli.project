c19ee038f1259c752df711232aa14147f31c1412
dOcOb <abg1418@gmail.com>
2022-12-22 11:29:31 +0900

    feat: Session에 장바구니 담기 전에 store open check

--- src/main/java/kh/deli/domain/member/store/service/StoreBasketService.java
+++ src/main/java/kh/deli/domain/member/store/service/StoreBasketService.java
@@ -4,10 +4,12 @@ import com.google.gson.Gson;
 import kh.deli.domain.member.order.service.OrderBasketService;
 import kh.deli.domain.member.store.dto.BasketDTO;
 import kh.deli.domain.member.store.dto.StoreBasketMenuRequestDTO;
+import kh.deli.global.util.checker.Checker;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,11 +19,14 @@ public class StoreBasketService {
 
     private final OrderBasketService basketService;
     private final Gson gson;
+    private final Checker checker;
 
-    public void setBasketInSession(HttpSession session, String newMenuJson) {
+    public void setBasketInSession(HttpSession session, String newMenuJson) throws ParseException {
         BasketDTO basket = (BasketDTO) session.getAttribute("basket");
+        StoreBasketMenuRequestDTO basketMenu = gson.fromJson(newMenuJson, StoreBasketMenuRequestDTO.class);
+        checker.storeBsTimeCheckToError(basketMenu.getStoreSeq());
+
         if (basket == null) {
-            StoreBasketMenuRequestDTO basketMenu = gson.fromJson(newMenuJson, StoreBasketMenuRequestDTO.class);
 
             List<StoreBasketMenuRequestDTO> menuList = new ArrayList<>();
             menuList.add(basketMenu);
