5f9de1aec67bcc5d2bb4baa69de4e3dd780d9b1c
dOcOb <abg1418@gmail.com>
2022-12-27 16:12:19 +0900

    feat: 회원가입 쿠폰 발행 final

--- src/main/java/kh/deli/domain/main/mapper/MainMemberCouponMapper.java
+++ src/main/java/kh/deli/domain/main/mapper/MainMemberCouponMapper.java
@@ -1,2 +1,14 @@
-package kh.deli.domain.main.mapper;public class MainMemberCouponMapper {
+package kh.deli.domain.main.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface MainMemberCouponMapper {
+    // 회원가입 쿠폰 발행
+    @Select("INSERT INTO MEMBER_COUPON VALUES(MC_SEQ.NEXTVAL, 4, #{accSeq}, SYSDATE, SYSDATE + (30 * 24 * 60 * 60 * 1000))")
+    void giveSignUpCp(@Param("accSeq") int accSeq);
 }
