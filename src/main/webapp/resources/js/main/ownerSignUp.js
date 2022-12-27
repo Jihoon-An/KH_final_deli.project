0189354839bb4524b7b84a0c3bd3c41f2fc548f3
dOcOb <abg1418@gmail.com>
2022-12-27 13:56:19 +0900

    feat: 사업자 회원가입 페이지 디자인

--- src/main/webapp/resources/js/main/ownerSignUp.js
+++ src/main/webapp/resources/js/main/ownerSignUp.js
@@ -469,3 +469,30 @@ $('#phone').keyup(function (event) {
     var _val = this.value.trim();
     this.value = autoHypenTel(_val);
 });
+//////////////// 눈 /////////////////
+const MIN_DURATION = 10;
+
+function makeSnowflake() {
+    const body = document.getElementById("body");
+    const snowflake = document.createElement("div");
+    const delay = Math.random() * 10;
+    const initialOpacity = Math.random();
+    const duration = Math.random() * 20 + MIN_DURATION;
+    snowflake.classList.add("snowflake");
+    snowflake.style.left = `${Math.random() * window.screen.width}px`;
+    snowflake.style.animationDelay = `${delay}s`;
+    snowflake.style.opacity = initialOpacity;
+    snowflake.style.animation = `fall ${duration}s linear`;
+
+    body.appendChild(snowflake);
+
+    setTimeout(()=> {
+        body.removeChild(snowflake);
+        makeSnowflake()
+    }, (duration + delay) * 1000);
+}
+
+for (let index = 0; index < 50; index++){
+    setTimeout(makeSnowflake, 500 * index);
+}
+////////////////////////////////////////
