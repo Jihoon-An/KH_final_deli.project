115b36dcd4664e759f2f881697a7cc9d803c0b77
yookyungmin <ykmykm4608@naver.com>
2023-01-03 18:11:13 +0900

    refactor: 식당추가 컨트롤러 서비스 수정

--- src/main/java/kh/deli/domain/owner/controller/StoreAddController.java
+++ src/main/java/kh/deli/domain/owner/controller/StoreAddController.java
@@ -28,35 +28,13 @@ public class StoreAddController {
         return "/owner/storeAdd";
     }
     @RequestMapping("/storeAdd")
-    public String add(StoreDTO dto, MultipartFile file, int acc_seq) throws IOException {
+    public String add(StoreDTO dto, MultipartFile file, int acc_seq) throws Exception {
 
         System.out.println("ACC"+acc_seq);
 //        System.out.println(dto.getStore_bsns_hours());
 //        System.out.println(dto.getStore_name());
 
-
-        int owner_seq = storeService.selectOwnerSeq(acc_seq); //acc_seq로 owner_seq 조히
-        System.out.println(owner_seq);
-        dto.setOwner_seq(owner_seq); //owner_seq dq입력
-
-
-        String realPath=session.getServletContext().getRealPath("/resources/img/store");
-        File filePath=new File(realPath);
-        if(!filePath.exists()) {
-            filePath.mkdir(); //파일업로드 폴더가 없다면 생성
-        }
-
-        if(file.getSize()!=0) {//파일 사이즈가0이 아니라면
-            String oriName=file.getOriginalFilename();
-            //겹치지 않게 이름을 만들어야함
-            String sysName= UUID.randomUUID()+"_"+oriName;
-            file.transferTo(new File(filePath+"/"+sysName));
-            System.out.println("파일있을떄");
-
-            dto.setStore_logo(sysName);
-        }
-
-      storeService.insertStore(dto);
+      storeService.insertStore(dto, file, acc_seq);
 
         return "redirect:/owner/store/list";
     }
